8963dad914e50455b570d7f79ac8a01b9b0360bb
Justin18C <93444670+Justin18C@users.noreply.github.com>
2023-06-23 15:23:59 +0200

    Update alphabet-sounds.js

--- alphabet-sounds.js
+++ alphabet-sounds.js
@@ -1,41 +1,31 @@
-/*const a = new Audio('a.mp3');
-const b = new Audio('b.mp3');
-const c = new Audio('c.mp3');
-const d = new Audio('d.mp3');
-const e = new Audio('e.mp3');
-const f = new Audio('f.mp3');
-const g = new Audio('g.mp3');
-const h = new Audio('h.mp3');
-const i = new Audio('i.mp3');
-const j = new Audio('j.mp3');
-const k = new Audio('k.mp3');
-const l = new Audio('l.mp3');
-const m = new Audio('m.mp3');
-const n = new Audio('n.mp3');
-const ñ = new Audio('ñ.mp3');
-const o = new Audio('o.mp3');
-const p = new Audio('p.mp3');
-const q = new Audio('q.mp3');
-const r = new Audio('r.mp3');
-const s = new Audio('s.mp3');
-const t = new Audio('t.mp3');
-const u = new Audio('u.mp3');
-const v = new Audio('v.mp3');
-const w = new Audio('w.mp3');
-const x = new Audio('x.mp3');
-const y = new Audio('y.mp3');
-const z = Audio('z.mp3');*/
+const a = new Audio('alphabet-sounds/a.mp3');
+const b = new Audio('alphabet-sounds/b.mp3');
+const c = new Audio('alphabet-sounds/c.mp3');
+const d = new Audio('alphabet-sounds/d.mp3');
+const e = new Audio('alphabet-sounds/e.mp3');
+const f = new Audio('alphabet-sounds/f.mp3');
+const g = new Audio('alphabet-sounds/g.mp3');
+const h = new Audio('alphabet-sounds/h.mp3');
+const i = new Audio('alphabet-sounds/i.mp3');
+const j = new Audio('alphabet-sounds/j.mp3');
+const k = new Audio('alphabet-sounds/k.mp3');
+const l = new Audio('alphabet-sounds/l.mp3');
+const m = new Audio('alphabet-sounds/m.mp3');
+const n = new Audio('alphabet-sounds/n.mp3');
+const ñ = new Audio('alphabet-sounds/ñ.mp3');
+const o = new Audio('alphabet-sounds/o.mp3');
+const p = new Audio('alphabet-sounds/p.mp3');
+const q = new Audio('alphabet-sounds/q.mp3');
+const r = new Audio('alphabet-sounds/r.mp3');
+const s = new Audio('alphabet-sounds/s.mp3');
+const t = new Audio('alphabet-sounds/t.mp3');
+const u = new Audio('alphabet-sounds/u.mp3');
+const v = new Audio('alphabet-sounds/v.mp3');
+const w = new Audio('alphabet-sounds/w.mp3');
+const x = new Audio('alphabet-sounds/x.mp3');
+const y = new Audio('alphabet-sounds/y.mp3');
+const z = new Audio('alphabet-sounds/z.mp3');
 
-const audioFiles = {
-    a: new Audio('a.mp3'),
-    b: new Audio('b.mp3'),
-    c: new Audio('c.mp3'),
-    d: new Audio('d.mp3')
-};
-  
-function playAudio(event) {
-    const audio = audioFiles[event];
-    if (audio) {
-      audio.play();
-    }
+function myFunction(audio) {
+    audio.play()
 }
